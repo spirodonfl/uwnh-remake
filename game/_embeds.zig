67773d34f7916bff113771cf30a8ef555d6dcc2b
Nate Diven <natediven1@gmail.com>
2024-01-24 18:58:34 -0700

    deleted generated _embeds.zig file and added it to .gitignore

--- game/_embeds.zig
+++
@@ -1,7 +0,0 @@
-pub const world_0 = @embedFile("binaries/npc.bin");
-pub const world_1 = @embedFile("binaries/player.bin");
-pub const world_2 = @embedFile("binaries/test_data.bin");
-pub const world_3 = @embedFile("binaries/world_0_layer_0.bin");
-pub const world_4 = @embedFile("binaries/world_0_layer_1.bin");
-pub const world_5 = @embedFile("binaries/world_0_layer_2.bin");
-pub const world_6 = @embedFile("binaries/world_0_size.bin");
