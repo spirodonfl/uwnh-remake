cee0efe672a39b2c9eecac257fb5436c91ec24dd
Spiro Floropoulos <spirodonfl@gmail.com>
2024-03-09 16:52:00 -0800

    trying to re-implement despawn

--- web/scripts_js_modules/multiplayer_host.js
+++ web/scripts_js_modules/multiplayer_host.js
@@ -186,7 +186,11 @@ export class MultiplayerHost extends HTMLElement {
             console.log('User wants to despawn');
             var user_despawned = false;
             for (var i = 0; i < this.ships_to_players.length; ++i) {
-                if (this.ships_to_player[i] !== undefined && this.ships_to_players[i] !== null && this.ships_to_players[i].username === e.data.user) {
+                if (
+                    this.ships_to_player[i]
+                    && this.ships_to_players[i] !== null
+                    && this.ships_to_players[i].username === e.data.user
+                ) {
                     this.ship_to_players[i] = null;
                     user_despawned = true;
                     // TODO: Reset this entities health and anything else we need. wait, should I?
