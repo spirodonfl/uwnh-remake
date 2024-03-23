edbf415167885e2e95daed3fa4f4e815bb708064
Spiro Floropoulos <spirodonfl@gmail.com>
2024-03-23 16:27:55 -0700

    Moving frame stuff in prep for major game.js updates

--- web/scripts/frames.js
+++ web/scripts/frames.js
@@ -1,3 +1,83 @@
+import { wasm } from './wasm.js';
+
+let current_time_stamp = new Date().getTime();
+let previous_time_stamp = 0;
+let frames_this_second = 0;
+let elapsed_ms = 0;
+let current_fps = 0;
+let then = performance.now();
+const interval = 1000 / 30; // Frames per second
+let delta = 0;
+function updateStats() {
+    ++frames_this_second;
+
+    previous_time_stamp = current_time_stamp;
+    current_time_stamp = new Date().getTime();
+    elapsed_ms += current_time_stamp - previous_time_stamp;
+    current_fps = 1000 / (current_time_stamp - previous_time_stamp);
+
+    // Update every second
+    if (elapsed_ms >= 1000)
+    {
+        var game_component = document.querySelector('game-component');
+        game_component.updateFPS(frames_this_second);
+        elapsed_ms -= 1000;
+        frames_this_second = 0;
+        game_component.updateElementCount();
+    }
+}
+function tick() {
+    let now = performance.now();
+    if (now - then >= interval - delta) {
+        delta = Math.min(interval, delta + now - then - interval);
+        then = now;
+        FRAMES.runOnFrames();
+        updateStats();
+
+        // TODO: Move this to game.js with runOnFrames()
+        // Alternatively, we could have a global event bus that we trigger
+        // especially since we're doing a lot of different things
+        // based on which components are active in the page
+        wasm.game_processTick();
+
+        if (wasm.diff_getLength() > 0) {
+            var length = wasm.diff_getLength();
+            for (var l = 0; l < length; ++l) {
+                var diff = wasm.diff_getData(l);
+                // TODO: Fix magic number here. This is also on Zig side
+                if (diff === 69) {
+                    var attacker_entity_id = wasm.diff_getData((l + 1));
+                    var attacker_entity_type = wasm.game_entityGetType(attacker_entity_id);
+                    var attackee_entity_id = wasm.diff_getData((l + 2));
+                    // TODO: referencing multiplayer-host-component stuff is weird here. global events maybe?
+                    if (attacker_entity_type < 3) {
+                        var multiplayer_host_element = document.querySelector('multiplayer-host-component');
+                        if (multiplayer_host_element) {
+                            multiplayer_host_element.incrementLeaderboard(attacker_entity_id, attackee_entity_id);
+                            if (attackee_entity_id === 7) {
+                                multiplayer_host_element.disableKraken();
+                            }
+                        }
+                    } else if (attacker_entity_type == 99) {                        
+                        var health = wasm.game_entityGetHealth(attackee_entity_id);
+                        if (health === 0) {
+                            var multiplayer_host_element = document.querySelector('multiplayer-host-component');
+                            if (multiplayer_host_element) {
+                                multiplayer_host_element.despawnUser(attackee_entity_id);
+                            }
+                        }
+                    }
+                    l += 2;
+                }
+            }
+            document.querySelector('game-component').renderGame();
+            document.querySelector('editor-component').renderViewportData();
+            wasm.diff_clearAll();
+        }
+    }
+    FRAMES.requestAnimationFrame();
+}
+
 export const FRAMES = {
     frameCallbacks: [],
     addRunOnFrames: function (frames, clearOnRun, callback) {
@@ -33,4 +113,7 @@ export const FRAMES = {
             }
         }
     },
+    requestAnimationFrame: function () {
+        requestAnimationFrame(tick);
+    }
 }
--- web/scripts/game.js
+++ web/scripts/game.js
@@ -10,80 +10,6 @@ import { getRandomKey } from './helpers.js';
 import { Inputs } from './inputs.js';
 import { Editor } from './editor.js';
 
-let current_time_stamp = new Date().getTime();
-let previous_time_stamp = 0;
-let frames_this_second = 0;
-let elapsed_ms = 0;
-let current_fps = 0;
-let then = performance.now();
-const interval = 1000 / 30; // Frames per second
-let delta = 0;
-function updateStats() {
-    ++frames_this_second;
-
-    previous_time_stamp = current_time_stamp;
-    current_time_stamp = new Date().getTime();
-    elapsed_ms += current_time_stamp - previous_time_stamp;
-    current_fps = 1000 / (current_time_stamp - previous_time_stamp);
-
-    // Update every second
-    if (elapsed_ms >= 1000)
-    {
-        var game_component = document.querySelector('game-component');
-        game_component.updateFPS(frames_this_second);
-        elapsed_ms -= 1000;
-        frames_this_second = 0;
-        game_component.updateElementCount();
-    }
-}
-function tick() {
-    let now = performance.now();
-    if (now - then >= interval - delta) {
-        delta = Math.min(interval, delta + now - then - interval);
-        then = now;
-        FRAMES.runOnFrames();
-        updateStats();
-
-        wasm.game_processTick();
-
-        if (wasm.diff_getLength() > 0) {
-            var length = wasm.diff_getLength();
-            for (var l = 0; l < length; ++l) {
-                var diff = wasm.diff_getData(l);
-                // TODO: Fix magic number here. This is also on Zig side
-                if (diff === 69) {
-                    var attacker_entity_id = wasm.diff_getData((l + 1));
-                    var attacker_entity_type = wasm.game_entityGetType(attacker_entity_id);
-                    var attackee_entity_id = wasm.diff_getData((l + 2));
-                    // TODO: referencing multiplayer-host-component stuff is weird here. global events maybe?
-                    if (attacker_entity_type < 3) {
-                        var multiplayer_host_element = document.querySelector('multiplayer-host-component');
-                        if (multiplayer_host_element) {
-                            multiplayer_host_element.incrementLeaderboard(attacker_entity_id, attackee_entity_id);
-                            if (attackee_entity_id === 7) {
-                                multiplayer_host_element.disableKraken();
-                            }
-                        }
-                    } else if (attacker_entity_type == 99) {                        
-                        var health = wasm.game_entityGetHealth(attackee_entity_id);
-                        if (health === 0) {
-                            var multiplayer_host_element = document.querySelector('multiplayer-host-component');
-                            if (multiplayer_host_element) {
-                                multiplayer_host_element.despawnUser(attackee_entity_id);
-                            }
-                        }
-                    }
-                    l += 2;
-                }
-            }
-            document.querySelector('game-component').renderGame();
-            document.querySelector('editor-component').renderViewportData();
-            wasm.diff_clearAll();
-        }
-    }
-    requestAnimationFrame(tick);
-}
-
 // TODO: Move this to ComponentGame in webcomponents folder
 export class Game extends HTMLElement {
     constructor() {
@@ -279,11 +205,14 @@ export class Game extends HTMLElement {
     connectedCallback() {
         this.render();
 
+        FRAMES.requestAnimationFrame();
+
         globals.EVENTBUS.addEventListener('event', (e) => {
             console.log('GAME EVENT', e);
         });
 
         globals.INPUTS = globals.INPUTS.concat(this.inputs);
+
         this.shadowRoot.getElementById('mode').innerText = globals.MODES[globals.MODE];
 
         // TODO: This is kinda weird to put here, it's not the multiplayer file, but we need it here otherwise nothing initializes
@@ -380,6 +309,7 @@ export class Game extends HTMLElement {
         xhr.send();
     }
 
+    // TODO: Move this to editor
     createStaticLayer(layer_id, image_path) {
         let camera_x = wasm.viewport_getCameraX();
         let camera_y = wasm.viewport_getCameraY();
@@ -397,6 +327,7 @@ export class Game extends HTMLElement {
         return static_layer;
     }
 
+    // TODO: Move this to zig
     randomizeKraken() {
         let kraken_entity_id = 7;
         let entity_layer = wasm.game_getCurrentWorldEntityLayer();
@@ -547,6 +478,7 @@ export class Game extends HTMLElement {
         });
     }
 
+    // TODO: These should probably in a game class
     moveCameraUp() {
         wasm.viewport_moveCameraUp();
         // TODO: Use the GLOBAL eventbus instead of directly calling these
@@ -713,8 +645,6 @@ export class Game extends HTMLElement {
             <div id="clickable_view"></div>
             <div id="view"></div>
         `;
-
-        requestAnimationFrame(tick);
     }
 }
 customElements.define('game-component', Game);
