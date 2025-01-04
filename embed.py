4846905e2dbd794eff5ba5c47000f491b2b474dc
Spiro Floropoulos <spirodonfl@gmail.com>
2025-01-04 14:07:59 -0800

    mr elf man is cool but rvice sucks ballz

--- embed.py
+++ embed.py
@@ -2,7 +2,7 @@ import base64
 import shutil
 
 # Copy the HTML file
-shutil.copy('wasm_game.html', 'wasm_game_embedded.html')
+shutil.copy('wasm_game.html', 'index.html')
 
 # Read the WASM file and convert to base64
 with open('wasm_game.wasm', 'rb') as wasm_file:
@@ -12,7 +12,7 @@ with open('atlas.png', 'rb') as atlas_file:
     atlas_base64 = base64.b64encode(atlas_file.read()).decode('utf-8')
 
 # Read the HTML file
-with open('wasm_game_embedded.html', 'r') as file:
+with open('index.html', 'r') as file:
     html_content = file.read()
 
 # Replace the placeholder
@@ -20,6 +20,6 @@ html_content = html_content.replace('<!-- WASM_FILE_HERE -->', wasm_base64)
 html_content = html_content.replace('<!-- ATLAS FILE HERE -->', atlas_base64)
 
 # Write the modified content back
-with open('wasm_game_embedded.html', 'w') as file:
+with open('index.html', 'w') as file:
     file.write(html_content)
 
