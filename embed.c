c5730bd71e1ef151bc2d4487f083e18a7d3c9a15
Spiro Floropoulos <spirodonfl@gmail.com>
2025-01-21 11:05:35 -0800

    embed in C vs python

--- embed.c
+++ embed.c
@@ -7,13 +7,15 @@
 static const char base64_table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 // Function to encode data to base64
-char* base64_encode(const unsigned char* data, size_t input_length, size_t* output_length) {
+char* base64_encode(const unsigned char* data, size_t input_length, size_t* output_length)
+{
     *output_length = 4 * ((input_length + 2) / 3);
     char* encoded_data = malloc(*output_length + 1);
     
     if (encoded_data == NULL) return NULL;
     
-    for (size_t i = 0, j = 0; i < input_length;) {
+    for (size_t i = 0, j = 0; i < input_length;)
+    {
         uint32_t octet_a = i < input_length ? data[i++] : 0;
         uint32_t octet_b = i < input_length ? data[i++] : 0;
         uint32_t octet_c = i < input_length ? data[i++] : 0;
@@ -27,7 +29,8 @@ char* base64_encode(const unsigned char* data, size_t input_length, size_t* outp
     }
     
     // Add padding if necessary
-    for (size_t i = 0; i < (3 - input_length % 3) % 3; i++) {
+    for (size_t i = 0; i < (3 - input_length % 3) % 3; i++)
+    {
         encoded_data[*output_length - 1 - i] = '=';
     }
     
@@ -36,9 +39,11 @@ char* base64_encode(const unsigned char* data, size_t input_length, size_t* outp
 }
 
 // Function to read entire file into memory
-unsigned char* read_file(const char* filename, size_t* length) {
+unsigned char* read_file(const char* filename, size_t* length)
+{
     FILE* file = fopen(filename, "rb");
-    if (!file) {
+    if (!file)
+    {
         printf("Could not open file: %s\n", filename);
         return NULL;
     }
@@ -48,7 +53,8 @@ unsigned char* read_file(const char* filename, size_t* length) {
     fseek(file, 0, SEEK_SET);
     
     unsigned char* buffer = malloc(*length);
-    if (!buffer) {
+    if (!buffer)
+    {
         fclose(file);
         return NULL;
     }
@@ -56,7 +62,8 @@ unsigned char* read_file(const char* filename, size_t* length) {
     size_t bytes_read = fread(buffer, 1, *length, file);
     fclose(file);
     
-    if (bytes_read != *length) {
+    if (bytes_read != *length)
+    {
         free(buffer);
         return NULL;
     }
@@ -64,11 +71,13 @@ unsigned char* read_file(const char* filename, size_t* length) {
     return buffer;
 }
 
-int main() {
+int main()
+{
     // Read WASM file
     size_t wasm_length;
     unsigned char* wasm_data = read_file("wasm_game.wasm", &wasm_length);
-    if (!wasm_data) {
+    if (!wasm_data)
+    {
         printf("Error reading WASM file\n");
         return 1;
     }
@@ -76,7 +85,8 @@ int main() {
     // Read Atlas file
     size_t atlas_length;
     unsigned char* atlas_data = read_file("atlas.png", &atlas_length);
-    if (!atlas_data) {
+    if (!atlas_data)
+    {
         printf("Error reading Atlas file\n");
         free(wasm_data);
         return 1;
@@ -87,7 +97,8 @@ int main() {
     char* base64_wasm = base64_encode(wasm_data, wasm_length, &base64_wasm_length);
     free(wasm_data);
     
-    if (!base64_wasm) {
+    if (!base64_wasm)
+    {
         printf("Error encoding WASM to base64\n");
         free(atlas_data);
         return 1;
@@ -98,7 +109,8 @@ int main() {
     char* base64_atlas = base64_encode(atlas_data, atlas_length, &base64_atlas_length);
     free(atlas_data);
     
-    if (!base64_atlas) {
+    if (!base64_atlas)
+    {
         printf("Error encoding Atlas to base64\n");
         free(base64_wasm);
         return 1;
@@ -107,7 +119,8 @@ int main() {
     // Copy HTML file with both injections
     FILE* source = fopen("wasm_game.html", "rb");
     FILE* dest = fopen("index.html", "wb");
-    if (!source || !dest) {
+    if (!source || !dest)
+    {
         printf("Error opening HTML files\n");
         free(base64_wasm);
         free(base64_atlas);
@@ -118,10 +131,12 @@ int main() {
     char* wasm_placeholder = "<!-- WASM_FILE_HERE -->";
     char* atlas_placeholder = "<!-- ATLAS_FILE_HERE -->";
 
-    while (fgets(buffer, sizeof(buffer), source)) {
+    while (fgets(buffer, sizeof(buffer), source))
+    {
         char* placeholder_pos;
         
-        if ((placeholder_pos = strstr(buffer, wasm_placeholder))) {
+        if ((placeholder_pos = strstr(buffer, wasm_placeholder)))
+        {
             // Write everything before the placeholder
             *placeholder_pos = '\0';
             fputs(buffer, dest);
@@ -130,7 +145,8 @@ int main() {
             // Write everything after the placeholder
             fputs(placeholder_pos + strlen(wasm_placeholder), dest);
         }
-        else if ((placeholder_pos = strstr(buffer, atlas_placeholder))) {
+        else if ((placeholder_pos = strstr(buffer, atlas_placeholder)))
+        {
             // Write everything before the placeholder
             *placeholder_pos = '\0';
             fputs(buffer, dest);
@@ -139,7 +155,8 @@ int main() {
             // Write everything after the placeholder
             fputs(placeholder_pos + strlen(atlas_placeholder), dest);
         }
-        else {
+        else
+        {
             fputs(buffer, dest);
         }
     }
